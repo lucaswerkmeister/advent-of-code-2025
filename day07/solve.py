ebccc4c624bf817db8bb65af42f0d48540296218
Lucas Werkmeister <mail@lucaswerkmeister.de>
2025-12-07 13:17:29 +0100

    Optimize day 7, part 2 a bit more
    Only keep the last line of each world in memory, rather than the whole
    history. This still won’t be enough – we need to not be “materializing”
    all these worlds at all, and avoid calculating the same line millions of
    times – but it still makes it go noticeably faster because it has to do
    a lot less memory management already.

--- day07/solve.py
+++ day07/solve.py
@@ -4,14 +4,14 @@ import sys
 for file in sys.argv[1:]:
     with open(file) as f:
         lines = [line.strip() for line in f.readlines()]
-    worlds = [[lines[0]]]
+    worlds = [lines[0]]
 
     for lineno in range(1, len(lines)):
         line = lines[lineno]
         beam_char = { 1: 'S' }.get(lineno, '|')
         new_worlds = []
         for world in worlds:
-            previous_line = world[-1]
+            previous_line = world
             beam_index = previous_line.index(beam_char)
             below_beam_char = line[beam_index]
             if below_beam_char == '.':
@@ -24,7 +24,7 @@ for file in sys.argv[1:]:
             else:
                 raise ValueError(f"Unknown char '{char}': {line}")
             for processed_line in processed_lines:
-                new_worlds.append(world + [processed_line])
+                new_worlds.append(processed_line)
         worlds = new_worlds
         print(f'line {lineno}/{len(lines)} done', file=sys.stderr)
 
