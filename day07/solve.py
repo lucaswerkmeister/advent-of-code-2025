596309896c5bc093d8b6874dd408d8397f0498a4
Lucas Werkmeister <mail@lucaswerkmeister.de>
2025-12-07 12:57:07 +0100

    Day 7, part 2 attempt
    It’s very slow towards the end; it *might* finish today but I think I’ll
    see if I can optimize it.

--- day07/solve.py
+++ day07/solve.py
@@ -4,23 +4,32 @@ import sys
 for file in sys.argv[1:]:
     with open(file) as f:
         lines = [line.strip() for line in f.readlines()]
-    processed_lines = [lines[0]]
+    worlds = [[lines[0]]]
 
-    beam_splits = 0
     for lineno in range(1, len(lines)):
         line = lines[lineno]
-        previous_line = processed_lines[lineno-1]
-        processed_line = line
-        for index, char in enumerate(line):
-            previous_char = previous_line[index]
-            if previous_char not in {'|', 'S'}:
-                continue
-            if char == '.':
-                processed_line = processed_line[:index] + '|' + processed_line[index+1:]
-            elif char == '^':
-                processed_line = processed_line[:index-1] + '|^|' + processed_line[index+2:]
-                beam_splits += 1
-        processed_lines.append(processed_line)
+        new_worlds = []
+        for world in worlds:
+            previous_line = world[-1]
+            processed_lines = None
+            for index, char in enumerate(line):
+                previous_char = previous_line[index]
+                if previous_char not in {'|', 'S'}:
+                    continue
+                if char == '.':
+                    processed_lines = [line[:index] + '|' + line[index+1:]]
+                elif char == '^':
+                    processed_lines = [
+                        line[:index-1] + '|^.' + line[index+2:],
+                        line[:index-1] + '.^|' + line[index+2:],
+                    ]
+                else:
+                    raise ValueError(f"Unknown char '{char}': {line}")
+                break  # only one beam can be active
+            assert processed_lines is not None, f"no tachyon beam found in previous line: {line}"
+            for processed_line in processed_lines:
+                new_worlds.append(world + [processed_line])
+        worlds = new_worlds
+        print(f'line {lineno}/{len(lines)} done', file=sys.stderr)
 
-    print('\n'.join(processed_lines), file=sys.stderr)
-    print(f'{file}: {beam_splits} beam splits')
+    print(f'{file}: {len(worlds)} worlds')
