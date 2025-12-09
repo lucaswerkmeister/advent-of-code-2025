bd1fa6a217543f9592ed69d542fc8680cd4c184a
Lucas Werkmeister <mail@lucaswerkmeister.de>
2025-12-09 01:46:58 +0100

    Extract add_pair_to_circuits() in preparation for part 2
    In part 2 I think I’ll need two loops that update circuits (assuming I
    still want to make the program print out part 1, which I do), so let’s
    start to split up the ginormous process() function.

--- day08/solve.py
+++ day08/solve.py
@@ -26,99 +26,7 @@ def process(file: str, num_connections: int) -> int:
     # circuits is a dict of sets; each set’s key is its smallest member
     circuits = {}
     for pair in connections:
-        # check the invariant out of paranoia
-        for box, circuit in circuits.items():
-            assert box == min(circuit)
-        box1, box2 = pair
-        branch = None
-        if box1 in circuits:
-            branch = 'box1 in circuits'
-            # update circuit
-            circuit = circuits[box1]
-            assert box1 in circuit
-            circuit.add(box2)
-            # find other circuit to potentially merge this one with
-            for other_box, other_circuit in circuits.items():
-                if other_box == box1:
-                    continue
-                if box2 in other_circuit:
-                    # merge them
-                    circuit.update(other_circuit)
-                    if box1 < other_box:
-                        # our circuit’s key is smaller, keep that index and drop the other
-                        del circuits[other_box]
-                    else:
-                        # other circuit’s key is smaller, drop ours
-                        del circuits[box1]
-                        # save merged circuit
-                        circuits[other_box] = circuit
-                    break
-        elif box2 in circuits:
-            branch = 'box2 in circuits'
-            # update circuit, reindex with box1 as newly-smallest member
-            circuit = circuits[box2]
-            circuit.add(box1)
-            assert box2 in circuit
-            del circuits[box2]
-            circuits[box1] = circuit
-            # find other circuit to potentially merge this one with
-            for other_box, other_circuit in circuits.items():
-                if other_box == box1:
-                    continue
-                if box1 in other_circuit:
-                    # merge them
-                    circuit.update(other_circuit)
-                    if box1 < other_box:
-                        # our circuit’s key is smaller, keep that index and drop the other
-                        del circuits[other_box]
-                    else:
-                        # other circuit’s key is smaller, drop ours
-                        del circuits[box1]
-                        # save merged circuit
-                        circuits[other_box] = circuit
-                    break
-        else:
-            branch = 'else'
-            for other_box, circuit in circuits.items():
-                # find other circuit to add to
-                if box1 in circuit or box2 in circuit:
-                    branch = 'box1 in circuit or box2 in circuit'
-                    # update circuit
-                    circuit.add(box1)
-                    circuit.add(box2)
-                    if box1 < other_box:
-                        branch += ', box1 < other_box'
-                        # reindex with box1 as newly-smallest member
-                        del circuits[other_box]
-                        circuits[box1] = circuit
-                    # find other circuit to potentially merge this one with
-                    for other_circuit_box, other_circuit in circuits.items():
-                        if other_circuit == circuit:
-                            continue
-                        if box1 in other_circuit or box2 in other_circuit:
-                            branch += ', box1 in other_circuit or box2 in other_circuit'
-                            # merge them
-                            circuit.update(other_circuit)
-                            if box1 < other_circuit_box or other_box < other_circuit_box:
-                                # our circuit’s key is smaller, keep that index and drop the other
-                                del circuits[other_circuit_box]
-                            else:
-                                # other circuit’s key is smaller, drop whichever was ours
-                                if box1 < other_box:
-                                    del circuits[box1]
-                                else:
-                                    del circuits[other_box]
-                                # save merged circuit
-                                circuits[other_circuit_box] = circuit
-                            break
-                    break
-            else:
-                branch = 'else else'
-                # no other circuit contains this connection, add new circuit
-                circuits[box1] = { box1, box2 }
-        assert box1 in {box for circuit in circuits.values() for box in circuit}
-        assert box2 in {box for circuit in circuits.values() for box in circuit}, \
-            f'branch is {branch}'
+        add_pair_to_circuits(pair, circuits)
 
     for box, circuit in circuits.items():
         assert box == min(circuit)
@@ -131,6 +39,102 @@ def process(file: str, num_connections: int) -> int:
     circuit_sizes = [len(circuit) for circuit in circuits.values()]
     circuit_sizes.sort(reverse=True)
     return functools.reduce(operator.mul, circuit_sizes[:3], 1)
+
+def add_pair_to_circuits(pair, circuits):
+    # check the invariant out of paranoia
+    for box, circuit in circuits.items():
+        assert box == min(circuit)
+    box1, box2 = pair
+    branch = None
+    if box1 in circuits:
+        branch = 'box1 in circuits'
+        # update circuit
+        circuit = circuits[box1]
+        assert box1 in circuit
+        circuit.add(box2)
+        # find other circuit to potentially merge this one with
+        for other_box, other_circuit in circuits.items():
+            if other_box == box1:
+                continue
+            if box2 in other_circuit:
+                # merge them
+                circuit.update(other_circuit)
+                if box1 < other_box:
+                    # our circuit’s key is smaller, keep that index and drop the other
+                    del circuits[other_box]
+                else:
+                    # other circuit’s key is smaller, drop ours
+                    del circuits[box1]
+                    # save merged circuit
+                    circuits[other_box] = circuit
+                break
+    elif box2 in circuits:
+        branch = 'box2 in circuits'
+        # update circuit, reindex with box1 as newly-smallest member
+        circuit = circuits[box2]
+        circuit.add(box1)
+        assert box2 in circuit
+        del circuits[box2]
+        circuits[box1] = circuit
+        # find other circuit to potentially merge this one with
+        for other_box, other_circuit in circuits.items():
+            if other_box == box1:
+                continue
+            if box1 in other_circuit:
+                # merge them
+                circuit.update(other_circuit)
+                if box1 < other_box:
+                    # our circuit’s key is smaller, keep that index and drop the other
+                    del circuits[other_box]
+                else:
+                    # other circuit’s key is smaller, drop ours
+                    del circuits[box1]
+                    # save merged circuit
+                    circuits[other_box] = circuit
+                break
+    else:
+        branch = 'else'
+        for other_box, circuit in circuits.items():
+            # find other circuit to add to
+            if box1 in circuit or box2 in circuit:
+                branch = 'box1 in circuit or box2 in circuit'
+                # update circuit
+                circuit.add(box1)
+                circuit.add(box2)
+                if box1 < other_box:
+                    branch += ', box1 < other_box'
+                    # reindex with box1 as newly-smallest member
+                    del circuits[other_box]
+                    circuits[box1] = circuit
+                # find other circuit to potentially merge this one with
+                for other_circuit_box, other_circuit in circuits.items():
+                    if other_circuit == circuit:
+                        continue
+                    if box1 in other_circuit or box2 in other_circuit:
+                        branch += ', box1 in other_circuit or box2 in other_circuit'
+                        # merge them
+                        circuit.update(other_circuit)
+                        if box1 < other_circuit_box or other_box < other_circuit_box:
+                            # our circuit’s key is smaller, keep that index and drop the other
+                            del circuits[other_circuit_box]
+                        else:
+                            # other circuit’s key is smaller, drop whichever was ours
+                            if box1 < other_box:
+                                del circuits[box1]
+                            else:
+                                del circuits[other_box]
+                            # save merged circuit
+                            circuits[other_circuit_box] = circuit
+                        break
+                break
+        else:
+            branch = 'else else'
+            # no other circuit contains this connection, add new circuit
+            circuits[box1] = { box1, box2 }
+    assert box1 in {box for circuit in circuits.values() for box in circuit}
+    assert box2 in {box for circuit in circuits.values() for box in circuit}, \
+        f'branch is {branch}'
+
 
 for file in sys.argv[1:]:
     connections = {
