f59bddf97c6a02fb1adb97c3e486e68dd97f805e
Lucas Werkmeister <mail@lucaswerkmeister.de>
2025-12-09 23:35:53 +0100

    Try to optimize a bit and print progress
    There must be an infinite loop somewhere, it’s stuck at 8/122760.

--- day09/solve.py
+++ day09/solve.py
@@ -2,6 +2,13 @@
 import math
 import sys
 
+OUTSIDE = 0
+INSIDE = 1
+UPPER_EDGE = 2
+LOWER_EDGE = 3
+LEFT_EDGE = 4
+RIGHT_EDGE = 5
+
 for file in sys.argv[1:]:
     red_tiles = []
     green_horizontals = {}
@@ -26,8 +33,12 @@ for file in sys.argv[1:]:
 
     max_part1_area = 0
     max_part2_area = 0
+    done = 0
+    total = len(red_tiles) * (len(red_tiles) - 1) // 2
     for i, tile1 in enumerate(red_tiles):
         for tile2 in red_tiles[i+1:]:
+            done += 1
+            print(f'{done}/{total}')
             x1, x2 = sorted([tile1[0], tile2[0]])
             y1, y2 = sorted([tile1[1], tile2[1]])
             dx = x2 - x1 + 1
@@ -44,79 +55,79 @@ for file in sys.argv[1:]:
             def is_legal_rectangle():
                 for x in range(x1 + 1, x2):
                     for y in range(y1 + 1, y2):
-                        x_area = 'outside'  # one of 'outside', 'upper edge', 'lower edge', 'inside'
+                        x_area = OUTSIDE  # one of OUTSIDE, UPPER_EDGE, LOWER_EDGE, INSIDE
                         for x_incr in range(x):
                             for v in green_verticals.get(x_incr, []):
                                 v_y1, v_y2 = v
                                 if not v_y1 <= y <= v_y2:
                                     continue
-                                if x_area == 'outside':
+                                if x_area == OUTSIDE:
                                     if y == v_y1:
-                                        x_area = 'upper edge'
+                                        x_area = UPPER_EDGE
                                     elif y == v_y2:
-                                        x_area = 'lower edge'
+                                        x_area = LOWER_EDGE
                                     else:
-                                        x_area = 'inside'
-                                elif x_area == 'upper edge':
+                                        x_area = INSIDE
+                                elif x_area == UPPER_EDGE:
                                     if y == v_y1:
-                                        x_area = 'outside'
+                                        x_area = OUTSIDE
                                     elif y == v_y2:
-                                        x_area = 'inside'
+                                        x_area = INSIDE
                                     else:
                                         raise ValueError(f'Unaligned edge? {x_incr=}, {v=}')
-                                elif x_area == 'lower edge':
+                                elif x_area == LOWER_EDGE:
                                     if y == v_y1:
-                                        x_area = 'inside'
+                                        x_area = INSIDE
                                     elif y == v_y2:
-                                        x_area = 'outside'
+                                        x_area = OUTSIDE
                                     else:
                                         raise ValueError(f'Unaligned edge? {x_incr=}, {v=}')
                                 else:
-                                    assert x_area == 'inside'
+                                    assert x_area == INSIDE
                                     if y == v_y1:
-                                        x_area = 'lower edge'
+                                        x_area = LOWER_EDGE
                                     elif y == v_y2:
-                                        x_area = 'upper edge'
+                                        x_area = UPPER_EDGE
                                     else:
-                                        x_area = 'outside'
-                        if x_area == 'outside':
+                                        x_area = OUTSIDE
+                        if x_area == OUTSIDE:
                             return False
-                        y_area = 'outside'  # one of 'outside', 'left edge', 'right edge', 'inside'
+                        y_area = OUTSIDE  # one of OUTSIDE, LEFT_EDGE, RIGHT_EDGE, INSIDE
                         for y_incr in range(y):
                             for h in green_horizontals.get(y_incr, []):
                                 h_x1, h_x2 = h
                                 if not h_x1 <= x <= h_x2:
                                     continue
-                                if y_area == 'outside':
+                                if y_area == OUTSIDE:
                                     if x == h_x1:
-                                        y_area = 'left edge'
+                                        y_area = LEFT_EDGE
                                     elif x == h_x2:
-                                        y_area = 'right edge'
+                                        y_area = RIGHT_EDGE
                                     else:
-                                        y_area = 'inside'
-                                elif y_area == 'left edge':
+                                        y_area = INSIDE
+                                elif y_area == LEFT_EDGE:
                                     if x == h_x1:
-                                        y_area = 'outside'
+                                        y_area = OUTSIDE
                                     elif x == h_x2:
-                                        y_area = 'inside'
+                                        y_area = INSIDE
                                     else:
                                         raise ValueError(f'Unaligned edge? {y_incr=}, {h=}')
-                                elif y_area == 'right edge':
+                                elif y_area == RIGHT_EDGE:
                                     if x == h_x1:
-                                        y_area = 'inside'
+                                        y_area = INSIDE
                                     elif x == h_x2:
-                                        y_area = 'outside'
+                                        y_area = OUTSIDE
                                     else:
                                         raise ValueError(f'Unaligned edge? {y_incr=}, {h=}')
                                 else:
-                                    assert y_area == 'inside'
+                                    assert y_area == INSIDE
                                     if x == h_x1:
-                                        y_area = 'right edge'
+                                        y_area = RIGHT_EDGE
                                     elif x == h_x2:
-                                        y_area = 'left edge'
+                                        y_area = LEFT_EDGE
                                     else:
-                                        y_area = 'outside'
-                        if y_area == 'outside':
+                                        y_area = OUTSIDE
+                        if y_area == OUTSIDE:
                             return False
                 return True
             if is_legal_rectangle():
