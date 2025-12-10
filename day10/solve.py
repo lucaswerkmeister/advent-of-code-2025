b97f7d540ef9eeaea3cbab64127665fe36e2702e
Lucas Werkmeister <mail@lucaswerkmeister.de>
2025-12-10 20:20:40 +0100

    Refactoring in preparation for day 10, part 2
    In part 2 we will need to press buttons more than once after all, so
    stop representing them as bits and start representing them as lists
    instead. Unfortunately this makes part 1 a lot slower (ca. 4 seconds!).

--- day10/solve.py
+++ day10/solve.py
@@ -1,32 +1,47 @@
 #!/usr/bin/env python3
+from functools import reduce
 import sys
+from typing import Literal
 
-type Diagram = int  # binary encoded
-type Button = int  # binary encoded
-type JoltageReq = set[int]
+type Diagram = list[Literal[0, 1]]
+type Button = list[Literal[0, 1]]
+type JoltageReq = list[int]
 type Machine = tuple[Diagram, list[Button], JoltageReq]
 
 def parse_diagram(diagram: str) -> Diagram:
     assert diagram.startswith('[') and diagram.endswith(']')
-    return sum(2**i for i, c in enumerate(diagram[1:-1]) if c == '#')
+    return [1 if c == '#' else 0 for c in diagram[1:-1]]
 
-assert parse_diagram('[.###.#]') == 46  # 2 + 4 + 8 + 32
+assert parse_diagram('[.###.#]') == [0, 1, 1, 1, 0, 1]
 
-def parse_button(button: str) -> Button:
+def parse_button(button: str, diagram: Diagram) -> Button:
     assert button.startswith('(') and button.endswith(')')
-    return sum(2**int(num) for num in button[1:-1].split(','))
+    nums = { int(num) for num in button[1:-1].split(',') }
+    return [1 if i in nums else 0 for i in range(len(diagram))]
 
-assert parse_button('(1,3)') == 10  # 8 + 2
+assert parse_button('(0,3,4)', [0, 1, 1, 1, 0, 1]) == [1, 0, 0, 1, 1, 0]
 
-def parse_joltage_req(joltage_req: str) -> JoltageReq:
+def parse_joltage_req(joltage_req: str, diagram: Diagram) -> JoltageReq:
     assert joltage_req.startswith('{') and joltage_req.endswith('}')
-    return { int(joltage) for joltage in joltage_req[1:-1].split(',') }
+    joltages = [int(joltage) for joltage in joltage_req[1:-1].split(',')]
+    assert len(joltages) == len(diagram)
+    return joltages
 
-assert parse_joltage_req('{3,5,4,7}') == { 3, 5, 4, 7 }
+assert parse_joltage_req('{10,11,11,5,10,5}', [0, 1, 1, 1, 0, 1]) == [10, 11, 11, 5, 10, 5]
+
+def xor_lists(*lists: list[list[int]]) -> list[int]:
+    return [reduce(lambda i1, i2: i1 ^ i2, elems, 0) for elems in zip(*lists)]
+
+assert xor_lists([0, 1, 0, 1], [0, 0, 1, 1]) == [0, 1, 1, 0]
+
+def add_lists(*lists: list[list[int]]) -> list[int]:
+    return [sum(elems) for elems in zip(*lists)]
+
+assert add_lists([1, 2, 3], [2, 4, 1]) == [3, 6, 4]
 
 def needed_buttons(machine: Machine) -> int:
     diagram, buttons, _ = machine
-    if diagram == 0:
+    if sum(diagram) == 0:
         return 0
 
     for button in buttons:
@@ -35,20 +50,20 @@ def needed_buttons(machine: Machine) -> int:
 
     for i, button1 in enumerate(buttons):
         for button2 in buttons[i+1:]:
-            if button1 ^ button2 == diagram:
+            if xor_lists(button1, button2) == diagram:
                 return 2
 
     for i, button1 in enumerate(buttons):
         for j, button2 in enumerate(buttons[i+1:]):
             for button3 in buttons[j+1:]:
-                if button1 ^ button2 ^ button3 == diagram:
+                if xor_lists(button1, button2, button3) == diagram:
                     return 3
 
     for i, button1 in enumerate(buttons):
         for j, button2 in enumerate(buttons[i+1:]):
             for k, button3 in enumerate(buttons[j+1:]):
                 for button4 in buttons[k+1:]:
-                    if button1 ^ button2 ^ button3 ^ button4 == diagram:
+                    if xor_lists(button1, button2, button3, button4) == diagram:
                         return 4
 
     for i, button1 in enumerate(buttons):
@@ -56,7 +71,7 @@ def needed_buttons(machine: Machine) -> int:
             for k, button3 in enumerate(buttons[j+1:]):
                 for l, button4 in enumerate(buttons[k+1:]):
                     for button5 in buttons[l+1:]:
-                        if button1 ^ button2 ^ button3 ^ button4 ^ button5 == diagram:
+                        if xor_lists(button1, button2, button3, button4, button5) == diagram:
                             return 5
 
     for i, button1 in enumerate(buttons):
@@ -65,7 +80,7 @@ def needed_buttons(machine: Machine) -> int:
                 for l, button4 in enumerate(buttons[k+1:]):
                     for m, button5 in enumerate(buttons[l+1:]):
                         for button6 in buttons[m+1:]:
-                            if button1 ^ button2 ^ button3 ^ button4 ^ button5 ^ button6 == diagram:
+                            if xor_lists(button1, button2, button3, button4, button5, button6) == diagram:
                                 return 6
 
     for i, button1 in enumerate(buttons):
@@ -75,7 +90,7 @@ def needed_buttons(machine: Machine) -> int:
                     for m, button5 in enumerate(buttons[l+1:]):
                         for n, button6 in enumerate(buttons[m+1:]):
                             for button7 in buttons[n+1:]:
-                                if button1 ^ button2 ^ button3 ^ button4 ^ button5 ^ button6 ^ button7 == diagram:
+                                if xor_lists(button1, button2, button3, button4, button5, button6, button7) == diagram:
                                     return 7
 
     for i, button1 in enumerate(buttons):
@@ -86,7 +101,7 @@ def needed_buttons(machine: Machine) -> int:
                         for n, button6 in enumerate(buttons[m+1:]):
                             for o, button7 in enumerate(buttons[n+1:]):
                                 for button8 in buttons[o+1:]:
-                                    if button1 ^ button2 ^ button3 ^ button4 ^ button5 ^ button6 ^ button7 ^ button8 == diagram:
+                                    if xor_lists(button1, button2, button3, button4, button5, button6, button7, button8) == diagram:
                                         return 8
 
     raise ValueError(f'Needs more than 8 buttons: {machine}')
@@ -99,8 +114,8 @@ for file in sys.argv[1:]:
             diagram_str, rest = parts[0], parts[1:]
             buttons_str, joltage_req_str = rest[:-1], rest[-1]
             diagram = parse_diagram(diagram_str)
-            buttons = [parse_button(button_str) for button_str in buttons_str]
-            joltage_req = parse_joltage_req(joltage_req_str)
+            buttons = [parse_button(button_str, diagram) for button_str in buttons_str]
+            joltage_req = parse_joltage_req(joltage_req_str, diagram)
             machines.append((diagram, buttons, joltage_req))
 
     total_presses = 0
