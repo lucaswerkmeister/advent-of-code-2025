0b2458542a739919d8cf607029a13e4283e068a1
Lucas Werkmeister <mail@lucaswerkmeister.de>
2025-12-12 10:10:16 +0100

    Extract function in preparation for part 2

--- day11/solve.py
+++ day11/solve.py
@@ -3,20 +3,14 @@ import sys
 
 type Machine = str
 
-for file in sys.argv[1:]:
-    outwards: dict[Machine, list[Machine]] = {}
-    with open(file) as f:
-        for line in f:
-            machine, others = line.strip().split(': ')
-            outwards[machine] = others.split(' ')
-
-    inwards: dict[Machine, list[Machine]] = {}
-    for machine, others in outwards.items():
-        for other in others:
-            inwards.setdefault(other, []).append(machine)
-
-    paths_to_out: dict[Machine, int] = { 'out': 1 }
-    worklist = ['out']
+def paths_from_to(
+        outwards: dict[Machine, list[Machine]],
+        inwards: dict[Machine, list[Machine]],
+        src: Machine,  # `from` is a keyword :<
+        dst: Machine,
+) -> int:
+    paths_to_dst: dict[Machine, int] = { dst: 1 }
+    worklist = [dst]
     downstreams: dict[Machine, set[Machine]] = {}
     cyclic: set[Machine] = set()
     while worklist:
@@ -31,12 +25,26 @@ for file in sys.argv[1:]:
                 continue
             other_downstreams.add(machine)
             other_downstreams.update(machine_downstreams)
-            paths = sum(paths_to_out.get(sibling, 0) for sibling in outwards[other])
-            if paths != paths_to_out.get(other):
-                paths_to_out[other] = paths
+            paths = sum(paths_to_dst.get(sibling, 0) for sibling in outwards[other])
+            if paths != paths_to_dst.get(other):
+                paths_to_dst[other] = paths
                 worklist.append(other)
 
-    paths_from_you_to_out = paths_to_out['you']
+    return paths_to_dst[src]
+
+for file in sys.argv[1:]:
+    outwards: dict[Machine, list[Machine]] = {}
+    with open(file) as f:
+        for line in f:
+            machine, others = line.strip().split(': ')
+            outwards[machine] = others.split(' ')
+
+    inwards: dict[Machine, list[Machine]] = {}
+    for machine, others in outwards.items():
+        for other in others:
+            inwards.setdefault(other, []).append(machine)
+
+    paths_from_you_to_out = paths_from_to(outwards, inwards, 'you', 'out')
     print(f'{file}: {paths_from_you_to_out} paths from {'you'!r} to {'out'!r}')
     if file == 'input.sample':
         assert paths_from_you_to_out == 5
